9f71ebbd9ebebf98ea11487093aa1b36531d1f6a
Fernando Alfredo Sánchez Valadez <fernandoa.sanchez@softtek.com>
2020-03-30 11:46:58 -0600

    first commit

---
+++ Controllers/currencyController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using CurrencyService.models;
+using CurrencyService.Models;
+
+namespace CurrencyService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class currencyController : ControllerBase
+    {
+        // api/currencyservice/
+
+        [Route("conversion")]
+        [HttpPost]
+        public async Task<double> GetExchangeRate(CurrencyChange currencyChange)
+        {
+            using (var client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("https://api.exchangeratesapi.io/latest");
+                var response = await client.GetAsync($"?base={currencyChange.CurrencyCode}&symbols={currencyChange.CurrencyType}&amount={currencyChange.Units}");
+
+                var stringResult = await response.Content.ReadAsStringAsync();
+                var dictResult = JsonConvert.DeserializeObject<dynamic>(stringResult);
+
+                var exchangeValue = dictResult.rates[$"{currencyChange.CurrencyType}"];
+                var convert = exchangeValue * currencyChange.Units;
+
+                return convert;
+
+            }
+        }
+
+        //[HttpGet] 
+        //[Route("")]
+        //public ActionResult GetRecomendation()
+        //{
+        //    HttpClient recomendation = new HttpClient();
+        //    recomendation.BaseAddress = new Uri("https://api.exchangeratesapi.io/latest");
+
+        //    var response = recomendation.GetAsync("");
+        //    response.Wait();
+        //    var items = response.Result.Content.ReadAsStringAsync().Result;
+        //    Class @class = new Class();
+            
+        //    var dictResult = JsonConvert.DeserializeObject<dynamic>(items);
+
+        //    Class.Rates rates = new Class.Rates();
+        //    rates.MXN = items.Where<double>(MX);
+
+        //    return Ok(dictResult);
+        //}
+
+    }
+}
---
+++ Models/CurrencyChange.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyService.models
+{
+    public class CurrencyChange
+    {
+        public string CurrencyCode { get; set; }
+        public int Units { get; set; }
+        public double Nano { get; set; }
+        public string CurrencyType { get; set; }
+
+    }
+}
